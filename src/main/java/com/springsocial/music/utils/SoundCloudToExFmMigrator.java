805793f43205964ea2af691e9251fdfbeaa779ed
Michael Lavelle <michael@lavelle.name>
2012-04-09 18:57:28 +0100

    Adding SoundCloudToExFmMigrator

---
+++ src/main/java/com/springsocial/music/utils/SoundCloudToExFmMigrator.java
@@ -0,0 +1,46 @@
+package com.springsocial.music.utils;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.social.exfm.api.ExFm;
+import org.springframework.social.exfm.api.impl.ExFmTemplate;
+import org.springframework.social.soundcloud.api.SoundCloud;
+import org.springframework.social.soundcloud.api.Track;
+import org.springframework.social.soundcloud.api.impl.SoundCloudTemplate;
+
+public class SoundCloudToExFmMigrator {
+
+	private String soundCloudApiKey;
+	private int soundCloudFetchPageSize = 50;
+	
+	public SoundCloudToExFmMigrator(String soundCloudApiKey)
+	{
+		this.soundCloudApiKey = soundCloudApiKey;
+	}
+	
+	public void loveSoundCloudFavoritesOnExFm(String soundCloudUserId,String exFmUserId,String exFmPassword)
+	{		
+		// Construct a new SoundCloudTemplate only authorized by api key, as user-level authorization is not needed
+		// to retrieve favorite tracks
+		SoundCloud soundCloud = new SoundCloudTemplate(soundCloudApiKey);
+		
+		// Construct a new ExFmTemplate authorized for a specific user, specifying api base url, username and password
+		ExFm exFm = new ExFmTemplate("http://ex.fm/api/v3",exFmUserId,exFmPassword);
+		
+		// Retrieve the first page of SoundCloud favorite tracks
+		Page<Track> soundCloudFavorites = soundCloud.usersOperations().userOperations(soundCloudUserId).getFavorites(new PageRequest(0,soundCloudFetchPageSize));
+		
+		// For each page of SoundCloud favorite tracks...
+		for (int pageNumber = 0; pageNumber < soundCloudFavorites.getTotalPages();pageNumber++)
+		{
+			// For each SoundCloud favorite track on current page, love the track on Ex.Fm using the stream url
+			for (Track soundCloudTrack : soundCloudFavorites)
+			{
+				exFm.songOperations().loveSongBySourceUrl(soundCloudTrack.getStreamUrl());
+			}
+			// Get next page of tracks from SoundCloud if available
+			if (soundCloudFavorites.hasNextPage()) soundCloudFavorites = soundCloud.usersOperations().userOperations(soundCloudUserId).getFavorites(new PageRequest(pageNumber + 1,soundCloudFetchPageSize));
+		}	
+	}
+	
+}
